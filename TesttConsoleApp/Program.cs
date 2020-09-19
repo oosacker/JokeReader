4c5660b851b118ba254d51a86f8992c7ff94666b
Natsuki Hasegawa <oosacker@gmail.com>
2020-09-19 12:13:10 +1200

    Add project files.

---
+++ TesttConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TesttConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jokes = readJokes();
+
+            Console.WriteLine("Press any key to get a joke...\n");
+
+            while (Console.ReadKey() != null)
+            {
+                Console.WriteLine(chooseJoke(jokes) + "\n");
+            }
+            
+        }
+
+        static string chooseJoke(string[] jokes)
+        {
+            Random ran = new Random();
+            int random = ran.Next(0, jokes.Length);
+            return jokes[random];
+        }
+
+        static string[] readJokes()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\apple\source\repos\TesttConsoleApp\TesttConsoleApp\jokes.txt");
+            return lines;
+        }
+    }
+}
